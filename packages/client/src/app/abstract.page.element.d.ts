b014310b0b50a9b4875b3bb27e3a1d848a232232
Matthieu Aubry <aubry@cervval.com>
2025-04-17 21:08:40 +0200

    Remove obsolete files

--- packages/client/src/app/abstract.page.element.d.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractWebComponent } from "../components/abstract.webcomponent";
-/**
- * Abstract class for a page. Same as AbstractWebComponent, but with a connectedCallback method.
- */
-export declare abstract class AbstractPageElement<Data> extends AbstractWebComponent<Data> {
-    /** Called when the component is inserted in the DOM */
-    connectedCallback(): void;
-}
