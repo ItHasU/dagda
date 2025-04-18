c3f2a2c276b2a76f2de1fb74604931f62ecd8c26
Matthieu Aubry <aubry@cervval.com>
2025-04-18 15:55:50 +0200

    Remove obsolete files

--- bootstrap/server/old/modules/config.ts
+++
@@ -1,8 +0,0 @@
-export type ENV_VARIABLES_STR =
-    "BASE_URL" |
-    "GOOGLE_CLIENT_ID" | "GOOGLE_CLIENT_SECRET" |
-    "API_URL" | "API_WOL_SCRIPT" | // User can pass a script to wake the server
-    "REPLICATE_TOKEN" | "REPLICATE_MODELS_SDXL" | "REPLICATE_MODELS_FLUX" |
-    "OPENAI_API_TOKEN" |
-    "DATABASE_URL";
-export type ENV_VARIABLES_NUMBER = "PORT";
